b7262d7ad2275b69e4f6c25d36c3af35fb81242b
Nicholas Lachhman <NLachhman@goeasy.com>
2022-07-14 16:44:37 +0000

    CNG-1 - Added dist

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault")["default"];
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = index;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Easybutton = require("../stories/Easybutton");
+
+function index() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement(_Easybutton.Easybutton, null));
+}
